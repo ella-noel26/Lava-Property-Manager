4a3c73fbac6d201e97d3332cd039ffa28b92fbe0
ella-noel26 <ella.n.frandsen@gmail.com>
2025-11-22 10:26:52 -0700

    Created fields, constructors and getters and setters for stay and issues classes.

--- src/Model/Issue.java
+++ src/Model/Issue.java
@@ -1,5 +1,76 @@
 package Model;
 
 public class Issue {
-    
+    private int[] reportedDate;
+    private int[] startedDate;
+    private int[] resolvedDate;
+    private String description;
+    private String title;
+
+    public Issue(String name, int[] reportDate, int[] startDate, int[] resolveDate, String desc){
+        this.title = name;
+        
+        this.reportedDate = new int[reportDate.length];
+        for (int index = 0; index < reportDate.length; index++){
+            this.reportedDate[index] = reportDate[index];
+        }
+
+        this.startedDate = new int[startDate.length];
+        for (int index = 0; index < startDate.length; index++){
+            this.startedDate[index] = startDate[index];
+        }
+
+        this.resolvedDate = new int[resolveDate.length];  
+        for (int index = 0; index < resolveDate.length; index++){
+            this.resolvedDate[index] = resolveDate[index];
+        }
+
+        this.description = desc;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String name){
+        this.title = name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String desc){
+        this.description = desc;
+    }
+
+    public int[] getReportedDate(){
+        return this.reportedDate;
+    }
+
+    public void setReportedDate(int[] date){
+        for (int index = 0; index < date.length; index++){
+            this.reportedDate[index] = date[index];
+        }
+    }
+
+    public int[] getStartedDate(){
+        return this.startedDate;
+    }
+
+    public void setStartedDate(int[] date){
+        for (int index = 0; index < date.length; index++){
+            this.startedDate[index] = date[index];
+        }
+    }
+
+    public int[] getResolvedDate(){
+        return this.resolvedDate;
+    }
+
+    public void setResolvedDate(int[] date){
+        for (int index = 0; index < date.length; index++){
+            this.resolvedDate[index] = date[index];
+        }
+    }
 }
--- src/Model/Stay.java
+++ src/Model/Stay.java
@@ -1,5 +1,60 @@
 package Model;
 
 public class Stay {
-    
+    private int[] startDate;
+    private int[] endDate;
+    double price;
+    int location; //Lodge is 1, bunkhouse is 2
+
+    public Stay(int[] arrivalDate, int[] leaveDate, double pricePaid, int place){
+        this.startDate = new int[arrivalDate.length];
+        for (int index = 0; index < arrivalDate.length; index++){
+            this.startDate[index] = arrivalDate[index];
+        }
+
+        this.endDate = new int[leaveDate.length];
+        for (int index = 0; index < leaveDate.length; index++){
+            this.endDate[index] = leaveDate[index];
+        }
+
+        this.price = pricePaid;
+        
+        this.location = place;
+    }
+
+    public int[] getStartDate(){
+        return this.startDate;
+    }
+
+    public void setStartDate(int[] date){
+        for (int index = 0; index < date.length; index++){
+            this.startDate[index] = date[index];
+        }
+    }
+
+    public int[] getEndDate(){
+        return this.endDate;
+    }
+
+    public void setEndDate(int[] date){
+        for (int index = 0; index < date.length; index++){
+            this.endDate[index] = date[index];
+        }
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(double num){
+        this.price = num;
+    }
+
+    public int getLocation(){
+        return this.location;
+    }
+
+    public void setLocation(int place){
+        this.location = place;
+    }
 }
