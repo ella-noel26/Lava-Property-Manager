5e02d3f48bb852b85427e2edd0d81d058dc74110
Ella <ella.n.frandsen@gmail.com>
2025-11-23 00:42:10 -0700

    Created window

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
+import javax.swing.JFrame;
+
 import Model.Model;
 public class App {
     public static void main(String[] args) throws Exception {
         Model model = new Model();
+        JFrame window = new JFrame("Lava Property Manager");
+        window.setBounds(100, 100, 300, 300);
+        window.setDefaultCloseOperation(1);
+        window.setVisible(true);
         System.out.println("Hello, World!");
     }
 }
